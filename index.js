f6678a9e9351d1281358066d8e9eb55ad3877149
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-10 14:30:31 +0530

    update the nanoid error

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express=require("express");
 const urlRoute=require("./routes/url")
+const {connectToMongodb}=require("./connect")
 const app=express();
 
 const PORT=3002;
+
+connectToMongodb('mongodb://localhost:27017/short-url').then(()=>{
+    console.log("mongodb connected succesfully")
+})
+
+
 
 app.use("/url",urlRoute);
 
