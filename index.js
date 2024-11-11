1602ddc9647b820aa7e71f001a6cf784a3118dcd
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-11 15:35:42 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express=require("express");
-const urlRoute=require("./routes/url")
-const {connectToMongodb}=require("./connect")
-const app=express();
+const express = require("express");
+const urlRoute = require("./routes/url");
+const { connectToMongodb } = require("./connect");
+const app = express();
+const URL = require("./models/url");
+const PORT = 3002;
 
-const PORT=3002;
+app.use(express.json());
+connectToMongodb("mongodb://localhost:27017/short-url").then(() => {
+  console.log("mongodb connected succesfully");
+});
 
-app.use(express.json())
-connectToMongodb('mongodb://localhost:27017/short-url').then(()=>{
-    console.log("mongodb connected succesfully")
-})
+app.use("/url", urlRoute);
 
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry=await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+            timestamp:Date.now()
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectUrl)
+});
 
-app.use("/url",urlRoute);
-
-app.listen(PORT,()=>{
-    console.log("server is listening at port 3002");
-})
+app.listen(PORT, () => {
+  console.log("server is listening at port 3002");
+});
