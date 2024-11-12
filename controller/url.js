9ceeb0227a0c886f98d298fc108099dd6d2dc406
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-12 15:25:13 +0530

    update tiny url project

--- controller/url.js
+++ controller/url.js
@@ -1,18 +1,28 @@
-const {nanoid}=require("nanoid");
-const URL=require("../models/url")
+const { nanoid } = require("nanoid");
+const URL = require("../models/url");
 
-async function handleGenerateNewShortURL(req,res){
-    const body=req.body;
-    if(!body.url) return res.status(400).json({error:"url is required"});
-    const shortId=nanoid(10);
-    await URL.create({
-        shortId:shortId,
-        redirectUrl:body.url,
-        visitHistory:{}
-    })
-    return res.json({id:shortId})
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortId = nanoid(10);
+  await URL.create({
+    shortId: shortId,
+    redirectUrl: body.url,
+    visitHistory: {},
+  });
+  return res.json({ id: shortId });
 }
 
-module.exports={
-    handleGenerateNewShortURL
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalclicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
+
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetAnalytics
+};
