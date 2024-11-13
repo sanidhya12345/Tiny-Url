4de9eff3446a3a684cce563b31d91bdedfa28d50
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-13 15:24:18 +0530

    update tiny url project

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const urlRoute = require("./routes/url");
 const { connectToMongodb } = require("./connect");
 const app = express();
 const URL = require("./models/url");
+const path = require("path");
+
 const PORT = 3002;
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use("/new", async (req, res) => {
+  const allUrl = await URL.find();
+  return res.render("home", {
+    urls: allUrl,
+  });
+});
 
 app.use(express.json());
 connectToMongodb("mongodb://localhost:27017/short-url").then(() => {
@@ -14,19 +25,21 @@ app.use("/url", urlRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
-  const entry=await URL.findOneAndUpdate(
+  const entry = await URL.findOneAndUpdate(
     {
       shortId,
     },
     {
       $push: {
         visitHistory: {
-            timestamp:Date.now()
+          timestamp: Date.now(),
         },
       },
     }
   );
-  res.redirect(entry.redirectUrl)
+  if (entry) {
+    res.redirect(entry.redirectUrl);
+  }
 });
 
 app.listen(PORT, () => {
